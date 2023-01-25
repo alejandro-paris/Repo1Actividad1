4a671a20c064742cf88e406a5d4502bd641e15c7
Mikel <mikelballes1@gmail.com>
2023-01-25 20:45:07 +0100

    Primer commit de Mikel Ballesteros

---
+++ ProyectoActividad1/src/mikel/Mikel.java
@@ -0,0 +1,10 @@
+package mikel;
+
+public class Mikel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Buenas Tardes, Mi nombre es Mikel");
+	}
+
+}
