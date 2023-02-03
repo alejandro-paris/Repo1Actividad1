9b156a024d53be0407eeb0292a217d24a89d8044
alejandro.paris <alejandroparisgarcia@gmail.com>
2023-02-03 17:50:38 +0100

    Añado comidas a la clase Maria

--- ProyectoActividad1/src/maria/Maria.java
+++ ProyectoActividad1/src/maria/Maria.java
@@ -9,6 +9,9 @@ public class Maria {
 		System.out.println("SENDERISMO");
 		System.out.println("NATACION");
 		System.out.println("JUEGOS DE MESA");
+		System.out.println("Ensalada");
+		System.out.println("Paella");
+		System.out.println("Ensaladilla rusa");
 		
 		
 	}
