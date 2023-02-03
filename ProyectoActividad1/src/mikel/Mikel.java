14e11748f685a44820d934b599c79a4487890543
Mikel <mikelballes1@gmail.com>
2023-02-03 16:57:02 +0100

    Los tres hobbies de Mikel

--- ProyectoActividad1/src/mikel/Mikel.java
+++ ProyectoActividad1/src/mikel/Mikel.java
@@ -5,6 +5,8 @@ public class Mikel {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Buenas Tardes, Mi nombre es Mikel");
+
+System.out.println("Me gusta ir al cine, hacer deporte y la lectura ");
 	}
 
 }
